25060c6af85f64904f01e01d5d5502463e73d8a0
catatsuy <catatsuy@catatsuy.org>
2013-07-13 16:46:10 +0900

    satos camera
    satos camera

--- src/lambda2.cc
+++ src/lambda2.cc
@@ -1,10 +1,8 @@
-#include <functional>
-
-int main() {
+int sample() {
   int a = 2;
   int b = 3;
   std::function<int ()> Sum =
       [=](int x, int y) { return x + y; };
   int c = Sum(a, b);
-  return 0;
+  return c;
 }
